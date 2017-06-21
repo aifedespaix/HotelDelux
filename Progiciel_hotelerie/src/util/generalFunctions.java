402eeb6f8f473b49a33a92efe7bf7e44b137ccfa
clapette <clapette01@gmail.com>
2017-06-21 13:32:30 +0200

    GeneralFunction Nettoyage

--- Progiciel_hotelerie/src/util/generalFunctions.java
+++ Progiciel_hotelerie/src/util/generalFunctions.java
@@ -4,38 +4,37 @@ import java.sql.Date;
 
 public class generalFunctions {
 
-	/**
-	 * Function qui calcule le prix d'une chambre en fonction du nombre d'enfants, adultes, des prix et du nombre de jours dans l'hotel
-	 * @param nbAdultes
-	 * @param nbEnfants
-	 * @param prixAdulte
-	 * @param prixEnfant
-	 * @param dateDebut
-	 * @param dateFin
-	 * @return
-	 */
-	
-	public static double calculPrixChambre(double prix, Date dateDebut, Date dateFin){
-		double prixReservation = 0;
-		final long MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
-		int nbJoursSejour = (int)((dateFin.getTime() - dateDebut.getTime()) / MILLISECONDS_IN_DAY);		
-		prixReservation = prix * nbJoursSejour;
-		
-		return prixReservation;
-	}
-	
-	public static String isValidate(Boolean valide){
-		String isValide = "";
-		if( valide == true){
-			isValide = "Valid�e";
-		} else {
-			isValide = "Non valid�e";
-		}
-		return isValide;
-	}
-	
-//	public static <T> T read(Class<T> theClass) {
-//			theClass.getClass();
-//		    return o;
-//		}
+    /**
+     * Function qui calcule le prix d'une chambre en fonction du nombre d'enfants, adultes, des prix et du nombre de jours dans l'hotel
+     *
+     * @param prix      double
+     * @param dateDebut Date
+     * @param dateFin   Date
+     * @return double
+     */
+
+    public static double calculPrixChambre(double prix, Date dateDebut, Date dateFin) {
+        double prixReservation;
+
+        final long MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
+        int nbJoursSejour = (int) ((dateFin.getTime() - dateDebut.getTime()) / MILLISECONDS_IN_DAY);
+        prixReservation = prix * nbJoursSejour;
+
+        return prixReservation;
+    }
+
+    /**
+     * @param valide Boolean
+     * @return String
+     */
+    public static String isValidate(Boolean valide) {
+        String isValide;
+        if (valide) {
+            isValide = "Validée";
+        } else {
+            isValide = "Non validée";
+        }
+        return isValide;
+    }
+
 }
