d2f5c3844c9afbdbfc267e17545ab4ff063dc112
thomas <thomas.vincentbtsnir@gmail.com>
2017-06-23 12:06:54 +0200

    BATCH mes couilles fini

--- Progiciel_hotelerie/dump_windows.bat
+++ Progiciel_hotelerie/dump_windows.bat
@@ -6,4 +6,7 @@ set /p chemin=
 set cheminProj=%~dp0
 cd "%chemin%"
 SET isodate=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
-mysqldump.exe -u root hotel > %cheminProj%dump\dump_%isodate%.sql
+set t=%time:~0,8%
+rem replace colons with dashes
+set t=%t::=-%
+mysqldump.exe -u root hotel > %cheminProj%dump\dump_%isodate%_%t%.sql
